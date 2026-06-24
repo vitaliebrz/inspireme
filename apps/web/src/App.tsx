ba60e7485976560c574076c1fad0c849f02f5867
Vitalie <vitalik.b159@gmail.com>
2026-06-24 12:28:45 +0300

    feat: initializare proiect Vite + React + TypeScript + Tailwind

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0f1117] text-[#f0f2f8]">
+      <h1 className="text-4xl font-bold text-[#f6a623]">
+        InspireMe
+      </h1>
+    </div>
+  )
+}
+
+export default App
